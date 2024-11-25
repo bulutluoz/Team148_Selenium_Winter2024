118e7d78792d31954a77008aef5bb8719b32aef8
bulutluoz <abbulutluoz@gmail.com>
2024-11-25 10:16:11 +0100

    day02 sonu

--- src/day02_driverMethodlari_webElementKullanimi/C02_DriverNavigateMethodlari.java
+++ src/day02_driverMethodlari_webElementKullanimi/C02_DriverNavigateMethodlari.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class C02_DriverNavigateMethodlari {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args)  {
 
         /*
             Bir test otomasyonu icin
@@ -30,13 +30,46 @@ public class C02_DriverNavigateMethodlari {
          */
 
         //System.setProperty("Webdriver.chrome.driver","kurulumDosyalari/chromedriver");
-        WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new ChromeDriver();
 
 
-        driver.get("https://www.testotomasyonu.com");
+        //2. Youtube ana sayfasina gidelim . https://www.youtube.com/
+        driver.get("https://www.youtube.com/");
+        //   ve url'in youtube icerdigini test edin
+        String expectedUrlIcerik = "youtube";
+        String actualUrl = driver.getCurrentUrl();
+
+        if (actualUrl.contains(expectedUrlIcerik)){
+            System.out.println("Youtube url testi PASSED");
+        }else System.out.println("Youtube url testi FAILED");
+
+        //3. Testotomasyonu sayfasina gidelim. https://www.testotomasyonu.com/
+        driver.get("https://www.testotomasyonu.com/");
+
+        //   Title'in "Test Otomasyonu" icerdigini test edin
+        String expectedTitleIcerik = "Test Otomasyonu";
+        String actualTitle = driver.getTitle();
 
 
-        Thread.sleep(3000);
+        if (actualTitle.contains(expectedTitleIcerik)){
+            System.out.println("Test otomasyonu title testi PASSED");
+        }else System.out.println("Test otomasyonu title testi FAILED");
+
+        //4. Tekrar YouTube’sayfasina donelim
+        //    driver.navigate().to();   driver.get() ile ayni islevi yerine getirir
+        driver.navigate().back();
+
+
+        //5. Yeniden testotomasyonu sayfasina gidelim
+        driver.navigate().forward();
+
+
+        //6. Sayfayi Refresh(yenile) yapalim
+        driver.navigate().refresh();
+
+        //7. Sayfayi kapatalim / Tum sayfalari kapatalim
+
+
         driver.quit();
     }
 }
