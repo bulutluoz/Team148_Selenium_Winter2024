9f34018d651c5380b8fe294d5abcf9690c623823
bulutluoz <abbulutluoz@gmail.com>
2024-11-25 10:46:17 +0100

    day02 sonu

---
+++ src/day02_driverMethodlari_webElementKullanimi/C04_DriverManageMethodlari.java
@@ -0,0 +1,27 @@
+package day02_driverMethodlari_webElementKullanimi;
+
+public class C04_DriverManageMethodlari {
+
+    public static void main(String[] args) {
+
+        /*
+            Test otomasyonu bizim kodlarimizin
+            bizim yerimize internette
+            Web sayfalarinda islem yapmasidir
+
+            Bizim yazdigimiz Java kodlarinin hizi ile
+
+            1- bilgisayarimiz
+            2- internetimiz
+            3- ve ulasmaya calistigimiz sayfanin
+            hizlari arasinda fark olur.
+
+            Bu hizlar arasindaki fark
+            kodlarimizin calisamamasina
+            veya testin FAILED olmasina sebep olur
+
+         */
+
+
+    }
+}
