425bc84d8da24beb925caaa572341a1b2175fa7d
bulutluoz <abbulutluoz@gmail.com>
2024-11-26 12:49:28 +0100

    Day03 sonu

--- src/day03_locators/C03_ByLinkText.java
+++ src/day03_locators/C03_ByLinkText.java
@@ -1,10 +1,9 @@
-package day03_locators_relativeLocators;
+package day03_locators;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 import java.util.List;
